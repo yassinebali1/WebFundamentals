be47dbee852640be3bd3dc8dd662ce57535c9573
yassine bali <ybali3385@gmail.com>
2024-10-07 14:50:57 +0200

    Loop Challenges

---
+++ Week_2/Day_1/Practice/Loop Challenges/Loop Challenges.js
@@ -0,0 +1,44 @@
+// 1 Print odds 1-20
+function Print odds (){
+
+    for (var i = 1; i <= 20; i++) {
+        if (i % 2 !== 0) {
+          console.log(i);
+        }
+      }
+      
+
+}
+// 2 Decreasing Multiples of 3
+function Decreasing Multiples(){
+    for(var i=100;i>=0;i--){
+        if(i % 3 === 0){
+            console.log(i);
+        }
+    }
+}
+// 3  Print the sequence 
+//mafhemtouche :p
+
+// 4 sigma
+function sigma(){
+    let sum = 0;
+    for (var  i = 1; i <= 100; i++){
+        sum += i;
+    }
+console.log(sum);
+    
+}
+// 5 Factorial
+function Factorial(){
+    product=0;
+    for(var i=1; i<=12;i++){
+        product *= i;
+    }
+    console.log(product);
+
+}
+
+
+
+  
