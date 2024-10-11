97f01701fea889f69e135593ba1318b8d5685f12
yassine bali <ybali3385@gmail.com>
2024-10-11 10:24:53 +0200

    Making Connections

--- Week_2/Day_2/Practice/Making Connections/script.js
+++ Week_2/Day_2/Practice/Making Connections/script.js
@@ -1,3 +1,14 @@
-function editpofile(element){
-    
+console.log("page loaded...");
+function change(id){
+    var element=document.querySelector(id) ;
+    element.innerText = "Jeremy Benth";
+}
+function deleteThis(id){
+    var element = document.querySelector(id);
+    element.remove() ;
+}
+function increase(x) {
+    var element = document.querySelector(".badge") ;
+    element.innerText=parseInt(element.innerText)+1 ; 
+
 }
